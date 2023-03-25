970b4a5e31fdec5076100124c86c6a0b4cad50a9
dreautall <109872040+dreautall@users.noreply.github.com>
2023-03-25 21:04:44 +0000

    Animations: FAB

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -48,15 +49,24 @@ class HomePageState extends State<HomePage>
     _tabController.addListener(_handleTabChange);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _newTransactionFab = FloatingActionButton(
-        onPressed: () => showDialog<void>(
-          context: context,
-          builder: (BuildContext context) => const Dialog.fullscreen(
-            child: TransactionPage(),
-          ),
-        ),
-        tooltip: S.of(context).formButtonTransactionAdd,
-        child: const Icon(Icons.add),
+      _newTransactionFab = OpenContainer(
+        openBuilder: (BuildContext context, Function closedContainer) {
+          return const TransactionPage();
+        },
+        openColor: Theme.of(context).cardColor,
+        closedColor: Theme.of(context).colorScheme.primaryContainer,
+        closedShape: Theme.of(context).floatingActionButtonTheme.shape ??
+            const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16.0))),
+        closedElevation:
+            Theme.of(context).floatingActionButtonTheme.elevation ?? 6,
+        closedBuilder: (BuildContext context, Function openContainer) {
+          return FloatingActionButton(
+            onPressed: () => openContainer(),
+            tooltip: S.of(context).formButtonTransactionAdd,
+            child: const Icon(Icons.add),
+          );
+        },
       );
 
       NavPageScaffold navScaffold = NavPageScaffold.of(context);

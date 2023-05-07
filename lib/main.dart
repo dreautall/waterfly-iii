dfc3288c469e44fdd04b0c448c4ab62c44e3bdb9
dreautall <109872040+dreautall@users.noreply.github.com>
2023-05-07 13:03:16 +0000

    logging: foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart';
+import 'package:logging/logging.dart';
 
 import 'package:waterflyiii/app.dart';
 
 void main() async {
   Intl.defaultLocale = await findSystemLocale();
+  Logger.root.level = kDebugMode ? Level.ALL : Level.INFO;
+  Logger.root.onRecord.listen((LogRecord record) {
+    debugPrint("${record.level.name}: ${record.time}: ${record.message}");
+    // :TODO: add to file/stack/whatever when debug is enabled
+  });
   return runApp(const WaterflyApp());
 }

bcdf3e98f436beb26d1c9acfe14e9a665caf05b2
dreautall <109872040+dreautall@users.noreply.github.com>
2023-09-16 21:10:13 +0000

    NetWortChart: improvements

--- lib/pages/home/main_charts/networth.dart
+++ lib/pages/home/main_charts/networth.dart
@@ -9,73 +9,106 @@ import 'package:waterflyiii/widgets/charts.dart';
 class NetWorthChart extends StatelessWidget {
   const NetWorthChart({
     super.key,
-    required this.earned,
-    required this.spent,
-    required this.balance,
+    required this.assets,
+    required this.liabilities,
   });
 
-  final Map<DateTime, double> earned;
-  final Map<DateTime, double> spent;
-  final Map<DateTime, double> balance;
+  final Map<DateTime, double> assets;
+  final Map<DateTime, double> liabilities;
 
   @override
   Widget build(BuildContext context) {
-    final List<LabelAmountChart> chartDataEarned = <LabelAmountChart>[];
-    final List<LabelAmountChart> chartDataSpent = <LabelAmountChart>[];
+    final List<LabelAmountChart> chartDataAssets = <LabelAmountChart>[];
+    final List<LabelAmountChart> chartDataLiabilities = <LabelAmountChart>[];
+    final Map<String, double> balance = <String, double>{};
     final List<LabelAmountChart> chartDataBalance = <LabelAmountChart>[];
     final List<ChartSeries<LabelAmountChart, String>> chartData =
         <ChartSeries<LabelAmountChart, String>>[];
 
-    earned.forEach((DateTime d, double v) => chartDataEarned
-        .add(LabelAmountChart(DateFormat(DateFormat.ABBR_MONTH).format(d), v)));
-    spent.forEach((DateTime d, double v) => chartDataSpent
-        .add(LabelAmountChart(DateFormat(DateFormat.ABBR_MONTH).format(d), v)));
-    balance.forEach((DateTime d, double v) => chartDataBalance
-        .add(LabelAmountChart(DateFormat(DateFormat.ABBR_MONTH).format(d), v)));
-
-    debugPrint("${earned.entries.first.key}: ${earned.entries.first.value}");
-    debugPrint("${spent.entries.first.key}: ${spent.entries.first.value}");
-    debugPrint("${balance.entries.first.key}: ${balance.entries.first.value}");
+    assets.forEach((DateTime d, double v) {
+      chartDataAssets.add(
+          LabelAmountChart(DateFormat(DateFormat.ABBR_MONTH).format(d), v));
+      balance[DateFormat(DateFormat.ABBR_MONTH).format(d)] = v;
+    });
+    liabilities.forEach((DateTime d, double v) {
+      chartDataLiabilities.add(LabelAmountChart(
+          DateFormat(DateFormat.ABBR_MONTH).format(d), v.abs()));
+      balance[DateFormat(DateFormat.ABBR_MONTH).format(d)] =
+          (balance[DateFormat(DateFormat.ABBR_MONTH).format(d)] ?? 0) + v;
+    });
+    balance.forEach(
+        (String d, double v) => chartDataBalance.add(LabelAmountChart(d, v)));
 
     chartData.add(
-      SplineSeries<LabelAmountChart, String>(
+      AreaSeries<LabelAmountChart, String>(
+        dataSource: chartDataAssets,
+        xValueMapper: (LabelAmountChart data, _) => data.label,
+        yValueMapper: (LabelAmountChart data, _) => data.amount,
+        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
+        color: Colors.green,
+        opacity: 0.4,
+      ),
+    );
+    chartData.add(
+      AreaSeries<LabelAmountChart, String>(
+        dataSource: chartDataLiabilities,
+        xValueMapper: (LabelAmountChart data, _) => data.label,
+        yValueMapper: (LabelAmountChart data, _) => data.amount,
+        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
+        color: Colors.red,
+        opacity: 0.4,
+      ),
+    );
+    chartData.add(
+      AreaSeries<LabelAmountChart, String>(
         dataSource: chartDataBalance,
         xValueMapper: (LabelAmountChart data, _) => data.label,
         yValueMapper: (LabelAmountChart data, _) => data.amount,
-        legendItemText: "Balance",
+        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
+        color: Colors.blue,
+        opacity: 0.4,
+      ),
+    );
+
+    chartData.add(
+      LineSeries<LabelAmountChart, String>(
+        dataSource: chartDataAssets,
+        xValueMapper: (LabelAmountChart data, _) => data.label,
+        yValueMapper: (LabelAmountChart data, _) => data.amount,
+        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
+        color: Colors.green,
+        markerSettings: const MarkerSettings(
+          isVisible: true,
+          borderWidth: 0,
+          color: Colors.green,
+        ),
+      ),
+    );
+    chartData.add(
+      LineSeries<LabelAmountChart, String>(
+        dataSource: chartDataLiabilities,
+        xValueMapper: (LabelAmountChart data, _) => data.label,
+        yValueMapper: (LabelAmountChart data, _) => data.amount,
+        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
+        color: Colors.red,
+        markerSettings: const MarkerSettings(
+          isVisible: true,
+          borderWidth: 0,
+          color: Colors.red,
+        ),
+      ),
+    );
+    chartData.add(
+      LineSeries<LabelAmountChart, String>(
+        dataSource: chartDataBalance,
+        xValueMapper: (LabelAmountChart data, _) => data.label,
+        yValueMapper: (LabelAmountChart data, _) => data.amount,
         animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
         color: Colors.blue,
         markerSettings: const MarkerSettings(
           isVisible: true,
-        ),
-      ),
-    );
-
-    chartData.add(
-      ColumnSeries<LabelAmountChart, String>(
-        dataSource: chartDataEarned,
-        xValueMapper: (LabelAmountChart data, _) => data.label,
-        yValueMapper: (LabelAmountChart data, _) => data.amount,
-        legendItemText: "Earned",
-        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
-        color: Colors.green,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(2),
-          topRight: Radius.circular(2),
-        ),
-      ),
-    );
-    chartData.add(
-      ColumnSeries<LabelAmountChart, String>(
-        dataSource: chartDataSpent,
-        xValueMapper: (LabelAmountChart data, _) => data.label,
-        yValueMapper: (LabelAmountChart data, _) => data.amount,
-        legendItemText: "Spent",
-        animationDuration: animDurationEmphasized.inMilliseconds.toDouble() * 2,
-        color: Colors.red,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(2),
-          bottomRight: Radius.circular(2),
+          borderWidth: 0,
+          color: Colors.blue,
         ),
       ),
     );

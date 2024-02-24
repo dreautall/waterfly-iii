697f9d7a6843d06ddbe2c25bb35e94d403c20c17
dreautall <109872040+dreautall@users.noreply.github.com>
2024-02-24 14:40:25 +0000

    [test] extension tests

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test/test.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz_data;
 
 import 'package:waterflyiii/generated/swagger_fireflyiii_api/firefly_iii.swagger.dart';
 
@@ -22,7 +24,7 @@ void main() {
     late CurrencyRead currency;
 
     setUp(() {
-      currency = CurrencyRead(
+      currency = const CurrencyRead(
         id: "1234",
         type: "TEST TYPE",
         attributes: Currency(
@@ -32,10 +34,6 @@ void main() {
           decimalPlaces: 3,
         ),
       );
-    });
-    test("zero", () {
-      expect(currency.zero(locale: "en"), "0.000");
-      expect(currency.zero(locale: "de"), "0,000");
     });
     group("fmt", () {
       test("locale = en", () {
@@ -59,6 +57,23 @@ void main() {
             "1.234,568\u{00A0}TEST");
       });
     });
+    test("zero", () {
+      expect(currency.zero(locale: "en"), "0.000");
+      expect(currency.zero(locale: "de"), "0,000");
+    });
+  });
+
+  group("TransactionType", () {
+    test("color", () {
+      expect(TransactionTypeProperty.deposit.color, Colors.green);
+      expect(
+          TransactionTypeProperty.swaggerGeneratedUnknown.color, Colors.black);
+    });
+    test("icon", () {
+      expect(TransactionTypeProperty.withdrawal.icon, Icons.arrow_back);
+      expect(TransactionTypeProperty.swaggerGeneratedUnknown.icon,
+          Icons.question_mark);
+    });
   });
 
   group("DateTimeExtension", () {
@@ -66,6 +81,7 @@ void main() {
     late DateTime dt2;
     late TimeOfDay tod1;
     late TimeOfDay tod2;
+
     setUp(() {
       dt1 = DateTime(2017, 9, 7, 17, 30, 15, 20, 25);
       dt2 = DateTime(2017, 9, 7);
@@ -87,6 +103,116 @@ void main() {
     test("getTimeOfDay", () {
       expect(dt1.setTimeOfDay(tod1).getTimeOfDay(), tod1);
       expect(dt2.setTimeOfDay(tod2).getTimeOfDay(), tod2);
+    });
+  });
+
+  group("TZDateTimeExtension", () {
+    late tz.TZDateTime dt1;
+    late tz.TZDateTime dt2;
+    late TimeOfDay tod1;
+    late TimeOfDay tod2;
+
+    setUp(() {
+      tz_data.initializeTimeZones();
+
+      dt1 = tz.TZDateTime(
+          tz.getLocation("America/Detroit"), 2017, 9, 7, 17, 30, 15, 20, 25);
+      dt2 = tz.TZDateTime(tz.getLocation("Europe/Paris"), 2017, 9, 7);
+      tod1 = const TimeOfDay(hour: 1, minute: 2);
+      tod2 = const TimeOfDay(hour: 23, minute: 59);
+    });
+    test("setTimeOfDay", () {
+      expect(TimeOfDay.fromDateTime(dt1.setTimeOfDay(tod1)), tod1);
+      expect(TimeOfDay.fromDateTime(dt2.setTimeOfDay(tod2)), tod2);
+    });
+    test("setTime", () {
+      expect(TimeOfDay.fromDateTime(dt1.setTime(hours: 1, minutes: 2)), tod1);
+      expect(TimeOfDay.fromDateTime(dt2.setTime(hours: 23, minutes: 59)), tod2);
+    });
+    test("clearTime", () {
+      expect(TimeOfDay.fromDateTime(dt1.clearTime()),
+          const TimeOfDay(hour: 0, minute: 0));
+    });
+    test("getTimeOfDay", () {
+      expect(dt1.setTimeOfDay(tod1).getTimeOfDay(), tod1);
+      expect(dt2.setTimeOfDay(tod2).getTimeOfDay(), tod2);
+    });
+  });
+
+  group("ListStringIgnoreCase", () {
+    late List<String> l;
+
+    setUp(() {
+      l = <String>["aa", "BB", "cC"];
+    });
+
+    test("containsIgnoreCase", () {
+      expect(l.containsIgnoreCase("aa"), isTrue);
+      expect(l.containsIgnoreCase("AA"), isTrue);
+      expect(l.containsIgnoreCase("Aa"), isTrue);
+      expect(l.containsIgnoreCase("aA"), isTrue);
+      expect(l.containsIgnoreCase("bb"), isTrue);
+      expect(l.containsIgnoreCase("BB"), isTrue);
+      expect(l.containsIgnoreCase("Bb"), isTrue);
+      expect(l.containsIgnoreCase("bB"), isTrue);
+      expect(l.containsIgnoreCase("cc"), isTrue);
+      expect(l.containsIgnoreCase("CC"), isTrue);
+      expect(l.containsIgnoreCase("Cc"), isTrue);
+      expect(l.containsIgnoreCase("cC"), isTrue);
+      expect(l.containsIgnoreCase("a"), isFalse);
+      expect(l.containsIgnoreCase("A"), isFalse);
+    });
+  });
+
+  group("StringIgnoreCase", () {
+    late String s;
+
+    setUp(() {
+      s = "aBc";
+    });
+
+    test("containsIgnoreCase", () {
+      expect(s.containsIgnoreCase("a"), isTrue);
+      expect(s.containsIgnoreCase("A"), isTrue);
+      expect(s.containsIgnoreCase("b"), isTrue);
+      expect(s.containsIgnoreCase("B"), isTrue);
+      expect(s.containsIgnoreCase("c"), isTrue);
+      expect(s.containsIgnoreCase("C"), isTrue);
+      expect(s.containsIgnoreCase("d"), isFalse);
+      expect(s.containsIgnoreCase("D"), isFalse);
+    });
+  });
+
+  group("AccountTypeFilterIcon", () {
+    test("icon", () {
+      expect(AccountTypeFilter.asset.icon, Icons.money_outlined);
+      expect(
+          AccountTypeFilter.swaggerGeneratedUnknown.icon, Icons.question_mark);
+    });
+  });
+
+  group("BillAmountAvg", () {
+    late Bill b1, b2, b3, b4, b5;
+
+    setUp(() {
+      b1 = Bill(
+        name: "TEST",
+        date: DateTime(2017, 9, 7, 17, 30, 15, 20, 25),
+        repeatFreq: BillRepeatFrequency.swaggerGeneratedUnknown,
+        amountMin: "10",
+        amountMax: "20",
+      );
+      b2 = b1.copyWith(amountMin: "0", amountMax: "10");
+      b3 = b1.copyWith(amountMin: "10", amountMax: "0");
+      b4 = b1.copyWith(amountMin: "asdf", amountMax: "-10");
+      b5 = b1.copyWith(amountMin: "1", amountMax: "2");
+    });
+    test("avgAmount", () {
+      expect(b1.avgAmount, 15);
+      expect(b2.avgAmount, 10);
+      expect(b3.avgAmount, 10);
+      expect(b4.avgAmount, 10);
+      expect(b5.avgAmount, 1.5);
     });
   });
 }

780622e07892826e97d7c3bde26813fca90a8f39
Jerry <limzming@gmail.com>
2024-04-17 09:32:13 +0800

    Show tags on transaction screen

--- lib/pages/home/transactions.dart
+++ lib/pages/home/transactions.dart
@@ -293,6 +293,7 @@ class _HomeTransactionsState extends State<HomeTransactions>
     String foreignText = "";
     String sourceName = "";
     String destinationName = "";
+    List<String> tags = List<String>.empty();
     late bool reconciled;
     for (TransactionSplit trans in transactions) {
       if (trans.categoryName?.isNotEmpty ?? false) {
@@ -309,6 +310,9 @@ class _HomeTransactionsState extends State<HomeTransactions>
       }
       if (trans.hasAttachments ?? false) {
         hasAttachments = true;
+      }
+      if ((trans.tags ?? List<String>.empty()).isNotEmpty) {
+        tags = trans.tags!;
       }
       amount += double.parse(trans.amount);
       if (trans.foreignAmount?.isNotEmpty ?? false) {
@@ -523,13 +527,51 @@ class _HomeTransactionsState extends State<HomeTransactions>
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
-          subtitle: RichText(
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            text: TextSpan(
-              style: Theme.of(context).textTheme.bodyMedium,
-              children: subtitle,
-            ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  children: subtitle,
+                ),
+              ),
+              if (tags.isNotEmpty) ...<Widget>[
+                Wrap(
+                  children: tags
+                      .map(
+                        (String tag) => Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.label_outline,
+                                  size: 16,
+                                ),
+                                const SizedBox(width: 5),
+                                Flexible(
+                                  child: RichText(
+                                    overflow: TextOverflow.fade,
+                                    text: TextSpan(
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium,
+                                        text: tag),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )
+              ]
+            ],
           ),
           isThreeLine: true,
           shape: const RoundedRectangleBorder(

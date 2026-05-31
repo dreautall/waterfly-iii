b12f8f9a1ed96a44e275bddf63335b7924d83b89
dreautall <109872040+dreautall@users.noreply.github.com>
2026-05-31 15:07:45 +0200

    further java package upgrades

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -90,9 +90,9 @@ flutter {
 
 // Added: flutter_local_notifications
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation("androidx.window:window:1.0.0")
-    implementation("androidx.window:window-java:1.0.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("androidx.window:window:1.5.1")
+    implementation("androidx.window:window-java:1.5.1")
 }
 
 val abiCodes = mapOf("x86_64" to 1, "armeabi-v7a" to 2, "arm64-v8a" to 3)

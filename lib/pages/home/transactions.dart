9cb2ea8bff6d6302d77338b93a1fc0e329f2db1a
dreautall <109872040+dreautall@users.noreply.github.com>
2026-08-24 16:24:12 +0200

    [Transactions] show tags in a new line
    This way, tags can expand further than the foreign account name.
    
    ref #828

--- lib/pages/home/transactions.dart
+++ lib/pages/home/transactions.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 
 import 'package:animations/animations.dart';
 import 'package:chopper/chopper.dart' show Response;
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter/services.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:provider/provider.dart';
 import 'package:waterflyiii/auth.dart';
 import 'package:waterflyiii/extensions.dart';
@@ -887,98 +887,100 @@ class _HomeTransactionsState extends State<HomeTransactions>
                   ),
                 ],
               ),
-              subtitle: Row(
+              subtitle: Column(
                 crossAxisAlignment: .start,
                 children: <Widget>[
-                  // Front part
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: .start,
-                      children: <Widget>[
-                        RichText(
-                          overflow: .ellipsis,
-                          maxLines: 2,
-                          text: TextSpan(
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            children: subtitle,
-                          ),
+                  Row(
+                    crossAxisAlignment: .start,
+                    children: <Widget>[
+                      // Front part
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: .start,
+                          children: <Widget>[
+                            RichText(
+                              overflow: .ellipsis,
+                              maxLines: 2,
+                              text: TextSpan(
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                children: subtitle,
+                              ),
+                            ),
+                          ],
                         ),
-                        if (!context.watch<SettingsProvider>().hideTags &&
-                            tags.isNotEmpty) ...<Widget>[
-                          Wrap(
-                            children: tags
-                                .map(
-                                  (String tag) => Card(
-                                    child: Padding(
-                                      padding: const .all(6.0),
-                                      child: Row(
-                                        mainAxisSize: .min,
-                                        children: <Widget>[
-                                          const Icon(
-                                            Icons.label_outline,
-                                            size: 16,
-                                          ),
-                                          const SizedBox(width: 5),
-                                          Flexible(
-                                            child: RichText(
-                                              overflow: .fade,
-                                              text: TextSpan(
-                                                style: Theme.of(
-                                                  context,
-                                                ).textTheme.bodyMedium,
-                                                text: tag,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
+                      ),
+                      // Trailing part
+                      RichText(
+                        textAlign: .end,
+                        maxLines: 1,
+                        text: TextSpan(
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          children: <InlineSpan>[
+                            if (reconciled)
+                              const WidgetSpan(
+                                baseline: .ideographic,
+                                alignment: .middle,
+                                child: Padding(
+                                  padding: .only(right: 2),
+                                  child: Icon(Icons.check),
+                                ),
+                              ),
+                            if (_filters.account != null)
+                              TextSpan(
+                                text: currency.fmt(balance),
+                                style: Theme.of(context).textTheme.bodyMedium!
+                                    .copyWith(
+                                      fontFeatures: const <FontFeature>[
+                                        .tabularFigures(),
+                                      ],
+                                    ),
+                              ),
+                            if (_filters.account == null)
+                              TextSpan(
+                                text: switch (transactions.first.type) {
+                                  .deposit => destinationName,
+                                  .openingBalance => "",
+                                  .reconciliation => "",
+                                  _ => sourceName,
+                                },
+                              ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  if (!context.watch<SettingsProvider>().hideTags &&
+                      tags.isNotEmpty) ...<Widget>[
+                    Wrap(
+                      children: tags
+                          .map(
+                            (String tag) => Card(
+                              child: Padding(
+                                padding: const .all(6.0),
+                                child: Row(
+                                  mainAxisSize: .min,
+                                  children: <Widget>[
+                                    const Icon(Icons.label_outline, size: 16),
+                                    const SizedBox(width: 5),
+                                    Flexible(
+                                      child: RichText(
+                                        overflow: .fade,
+                                        text: TextSpan(
+                                          style: Theme.of(
+                                            context,
+                                          ).textTheme.bodyMedium,
+                                          text: tag,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                )
-                                .toList(),
-                          ),
-                        ],
-                      ],
-                    ),
-                  ),
-                  // Trailing part
-                  RichText(
-                    textAlign: .end,
-                    maxLines: 1,
-                    text: TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      children: <InlineSpan>[
-                        if (reconciled)
-                          const WidgetSpan(
-                            baseline: .ideographic,
-                            alignment: .middle,
-                            child: Padding(
-                              padding: .only(right: 2),
-                              child: Icon(Icons.check),
-                            ),
-                          ),
-                        if (_filters.account != null)
-                          TextSpan(
-                            text: currency.fmt(balance),
-                            style: Theme.of(context).textTheme.bodyMedium!
-                                .copyWith(
-                                  fontFeatures: const <FontFeature>[
-                                    .tabularFigures(),
                                   ],
                                 ),
-                          ),
-                        if (_filters.account == null)
-                          TextSpan(
-                            text: switch (transactions.first.type) {
-                              .deposit => destinationName,
-                              .openingBalance => "",
-                              .reconciliation => "",
-                              _ => sourceName,
-                            },
-                          ),
-                      ],
+                              ),
+                            ),
+                          )
+                          .toList(),
                     ),
-                  ),
+                  ],
                 ],
               ),
               isThreeLine: true,

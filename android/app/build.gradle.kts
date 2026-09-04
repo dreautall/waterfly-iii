674d48eed14a94037ee28ca43a56c466a3efc61f
dreautall <109872040+dreautall@users.noreply.github.com>
2026-09-04 22:13:24 +0200

    lower minSdk again, as there seems no reason to raise it that high.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.dreautall.waterflyiii"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 34
+        minSdk = 28
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
